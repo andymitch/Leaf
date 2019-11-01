69bcc22f65510cbf87b5acd116425a729c3d3983
neilborde <43529300+neilborde@users.noreply.github.com>
2019-11-01 12:04:05 -0600

    forgot pass

--- client/components/ForgotPassword.js
+++ client/components/ForgotPassword.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-import {Button, StyleSheet, Text, View, TextInput, Switch, Image, TouchableHighlight, StatusBar, ImageBackground} from 'react-native'
+import {Button, StyleSheet, Text, View, TextInput, Switch, Image, TouchableHighlight, TouchableOpacity, StatusBar, ImageBackground} from 'react-native'
 import {styles} from '../styles/style';
+import {FontAwesomeIcon as Icon} from '@fortawesome/react-native-fontawesome'
+import {faArrowLeft} from '@fortawesome/free-solid-svg-icons'
 
 
 export default class ForgotPassword extends Component{
@@ -11,18 +13,27 @@ state = {
 
   render() {
     return(
-      <View style={{flex: 2, justifyContent: 'center'}}>
-        <Text>Enter your recovery e-mail address:</Text>
-        <TextInput
-            onChangeText={recovery_email => this.setState({recovery_email: recovery_email})}
-            style={styles.input}
-            autoCapitalize='none'
-            placeholder='e-mail address'
-            placeholderTextColor='#999'
-            returnKeyType='done'
-            textContentType='emailAddress'
-        />
-        <Button style={styles.btn} title="Submit" onPress={() => this.props.navigation.pop()}/>
+      <View style={styles.container}>
+        <View style={{zIndex: 1,top: 100,justifyContent: "flex-start"}}>
+            <TouchableOpacity onPress={() => this.props.navigation.pop()}>
+                <Icon icon={faArrowLeft} style={{color: 'white'}} size={30}/>
+            </TouchableOpacity>
+        </View>
+        <ImageBackground source={require('../assets/img/leaf_background.jpg')} style={{width: '100%', height: '100%'}}>
+          <View style={[styles.login,{width: 300}]}>
+            <Text>Enter your recovery e-mail address:</Text>
+            <TextInput
+                onChangeText={recovery_email => this.setState({recovery_email: recovery_email})}
+                style={styles.input}
+                autoCapitalize='none'
+                placeholder='e-mail address'
+                placeholderTextColor='#999'
+                returnKeyType='done'
+                textContentType='emailAddress'
+            />
+            <Button style={styles.btn} title="Submit" onPress={() => this.props.navigation.pop()}/>
+          </View>
+        </ImageBackground>
       </View>
     )
   }
